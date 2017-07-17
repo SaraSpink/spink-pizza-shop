d1619bfe213c17bbad8cad5c7982785795b5e97c
Sara Spink <spinkbot@gmail.com>
2017-07-16 19:00:38 -0700

    continue working on the js

--- js/scripts.js
+++ js/scripts.js
@@ -1,54 +1,38 @@
 //Business Logic
 
 function pizzaOrder(toppings, size) {
-  this.toppings = toppings;
   this.size = size;
+  this.toppings = toppings;
 }
 
-pizzaOrder.prototype.toppings = function() {
-  $(function())
-}
-
-
+pizzaOrder.prototype.price = function() {
+  var price = 0;
+  if (this.size === "8") {
+    this.price = 10;
+  } else if (this.size === "10") {
+    this.price = 12;
+  } else if (this.size === "12") {
+    this.price = 14;
+  } else {
+    this.price = 16;
+  }
 
 //User Interface Logic
+
 $(document).ready(function(){
   $("form#pizzaSelections").submit(function(event){
     event.preventDefault();
-    $("#pizzaResult").show();
-
-    var toppingsCounter = 0
-    $("input:checkbox[name=toppings]:checked").each(function(){
-      toppingsCounter += 1;
-      if (toppingsCounter !== 3) {
-        return $('#pizzaResult').append("Congratulations you won a free pizza");
-      } else {
-        return $('#pizzaResult').append("3 toppings, your pizza is 10USD");
-      }
-    });
-
-    $("#health-responses").show();
-    $("input:checkbox[name=health-symptons]:checked").each(function(){
-      var healthSymptoms = $(this).val();
-      $('#health-responses').append(healthSymptoms + "<br>");
-      // userResponses.push(funTransportationMode);
-    });
-
-    $("#coping-responses").show();
-    $("input:checkbox[name=coping]:checked").each(function(){
-      var coping = $(this).val();
-      $('#coping-responses').append(coping + "<br>");
-    });
-    // var userResponses = [];
-    // $("input:checkbox[name=work-transportation]:checked").each(function(){
-    //   var workTransportationMode = $(this).val();
-    //   userResponses.push(workTransportationMode);
-    // // });
-    // $("input:checkbox[name=fun-transportation]:checked").each(function(){
-    //   var funTransportationMode = $(this).val();
-    //   userResponses.push(funTransportationMode);
-    // });
-    console.log(toppingsCounter);
-    $('#pizzaSelections').hide();
-  });
-});
+    var size = $(select#pizzaSize).val();
+    var toppings = $("input:checkbox[name=toppings]:checked").length;
+    var
+//     $("#pizzaResult").show();
+//
+//     var toppingsCounter = 0
+//     $("input:checkbox[name=toppings]:checked").each(function(){
+//       toppingsCounter += 1;
+//       if (toppingsCounter !== 3) {
+//         return $('#pizzaResult').append("Congratulations you won a free pizza");
+//       } else {
+//         return $('#pizzaResult').append("3 toppings, your pizza is 10USD");
+//       }
+//     });
