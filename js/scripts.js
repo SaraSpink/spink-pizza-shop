dab7029dfa1f362c2d12aae0da290b5be4237e40
Sara Spink <spinkbot@gmail.com>
2017-07-16 21:32:21 -0700

    keep trying to get the js working

--- js/scripts.js
+++ js/scripts.js
@@ -6,33 +6,25 @@ function pizzaOrder(toppings, size) {
 }
 
 pizzaOrder.prototype.price = function() {
-  var price = 0;
+  var price = 0
   if (this.size === "8") {
-    this.price = 10;
-  } else if (this.size === "10") {
-    this.price = 12;
-  } else if (this.size === "12") {
-    this.price = 14;
-  } else {
-    this.price = 16;
+      price = 10 + (this.toppings);
+    } else if (this.size === "10") {
+      price = 12 + (this.toppings);
+    } else if (this.size === "12") {
+      price = 14 + (this.toppings);
+    } else {
+      price = 16 + (this.toppings);
+    }
   }
 
-//User Interface Logic
+// //User Interface Logic
 
-$(document).ready(function(){
+$(document).ready(function() {
   $("form#pizzaSelections").submit(function(event){
     event.preventDefault();
-    var size = $(select#pizzaSize).val();
-    var toppings = $("input:checkbox[name=toppings]:checked").length;
-    var
-//     $("#pizzaResult").show();
-//
-//     var toppingsCounter = 0
-//     $("input:checkbox[name=toppings]:checked").each(function(){
-//       toppingsCounter += 1;
-//       if (toppingsCounter !== 3) {
-//         return $('#pizzaResult').append("Congratulations you won a free pizza");
-//       } else {
-//         return $('#pizzaResult').append("3 toppings, your pizza is 10USD");
-//       }
-//     });
+
+    $("input:checkbox[name=toppings]:checked").each(function() {
+     toppingsSelected += 1;
+     var numberOfToppings = $(this).val();
+});
