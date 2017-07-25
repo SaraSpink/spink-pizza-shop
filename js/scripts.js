fa8ce9b98d07a680ac51407c17083188ce32cab3
Sara Spink <spinkbot@gmail.com>
2017-07-25 10:59:39 -0700

    fix the logic and get the show and hide feature working

--- js/scripts.js
+++ js/scripts.js
@@ -8,15 +8,16 @@ function pizzaOrder(size, toppings) {
 pizzaOrder.prototype.price = function() {
   var price = 0
   if (this.size === "8") {
-      price = 10 + (this.toppings);
+      price = 10 + (this.toppings.length);
     } else if (this.size === "10") {
-      price = 12 + (this.toppings);
+      price = 12 + (this.toppings.length);
     } else if (this.size === "12") {
-      price = 14 + (this.toppings);
+      price = 14 + (this.toppings.length);
     } else {
-      price = 16 + (this.toppings);
+      price = 16 + (this.toppings.length);
     }
-  }
+  return price;
+}
 
 // //User Interface Logic
 
@@ -24,10 +25,15 @@ $(document).ready(function() {
   $("form#pizzaSelections").submit(function(event){
     event.preventDefault();
     var size = $("#pizzaSize").val();
-    var toppings = $("#pizzaSelections input:checked");
+    var toppings = [];
+    $("#pizzaSelections input:checked").each(function() {
+      toppings.push($(this).val());
+    })
     var toppingsCount = parseInt(toppings.length);
     var userOrder = new pizzaOrder (size, toppings);
     var price = userOrder.price();
+
+
 
     $("#yourPrice").append("$" + price);
     $("#pizzaResult").show();
